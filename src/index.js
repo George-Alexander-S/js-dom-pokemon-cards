e86af24e4e9792a0b1505bf836833aa31d16da9c
George <george.alexander.saav@gmail.com>
2024-09-26 11:50:35 +0200

    Core done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 
-console.log(data);
+// console.log(data);
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+function cardloop() {
+    data.map((d) => {
+        // console.log(d)
+        li = document.createElement("li")
+        li.classList.add('card')
+
+        h2 = document.createElement("h2")
+        h2.classList.add('card--title')
+        h2.innerHTML = d.name.charAt(0).toUpperCase() + d.name.slice(1)
+        li.appendChild(h2)
+        
+        img = document.createElement("img")
+        img.classList.add('card--img')
+        img.style.cssText = 'width:256px;'
+        img.src = d.sprites.front_default;
+        li.appendChild(img)
+
+        ul = document.createElement("ul")
+        ul.classList.add('card--text')
+
+        for (const specs of d.stats){
+            console.log(specs)
+            stat = document.createElement("li")
+            stat.innerHTML = specs.base_stat + ' ' + specs.stat.name.toUpperCase()
+            ul.appendChild(stat)
+        }
+        li.appendChild(ul)
+
+
+        document.getElementById("cards").appendChild(li)
+    });
+}
+// console.log(data[0]);
+cardloop();
